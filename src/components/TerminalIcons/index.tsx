fbc8703e69137292d8828dd496eafe3bbc37e459
Nicolas Vazquez <nicorvazquezs@gmail.com>
2024-07-02 15:09:36 -0300

    Styles to terminal header icons

--- src/components/TerminalIcons/index.tsx
+++ src/components/TerminalIcons/index.tsx
@@ -3,12 +3,37 @@ import MaximizeIcon from "@/assets/icons/icon-maximize-terminal.svg";
 import MinimizeIcon from "@/assets/icons/icon-minimize-terminal.svg";
 import CloseIcon from "@/assets/icons/icon-close-terminal.svg";
 
+const icons = [
+  {
+    src: MinimizeIcon,
+    alt: "Minimize",
+  },
+  {
+    src: MaximizeIcon,
+    alt: "Maximize",
+  },
+  {
+    src: CloseIcon,
+    alt: "Close",
+    customClass: styles.icons__iconContainer_close,
+  },
+];
+
 export const TerminalIcons = () => {
   return (
     <div className={styles.icons}>
-      <img src={MinimizeIcon} alt="Minimize" className={styles.icon} />
-      <img src={MaximizeIcon} alt="Maximize" className={styles.icon} />
-      <img src={CloseIcon} alt="Close" className={styles.icon} />
+      {icons.map((icon, index) => (
+        <div
+          key={index}
+          className={`${styles.icons__iconContainer} ${icon.customClass}`}
+        >
+          <img
+            src={icon.src}
+            alt={icon.alt}
+            className={styles.icons__iconContainer__icon}
+          />
+        </div>
+      ))}
     </div>
   );
 };
